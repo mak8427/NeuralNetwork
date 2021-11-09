74de5a4de6876b12dff711925c34558084ac3f15
mak <davide.mattioli.2001@gmail.com>
2021-11-09 10:22:41 +0100

    loss

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import nnfs
 import numpy as np
 import matplotlib.pyplot as plt
 from nnfs.datasets import spiral_data
+import math
+import time
+
 nnfs.init()
 np.random.seed(0)
 
-X,y= spiral_data(100,3)
 
+E=math.e
 
 class Layer_Dense:
     def __init__(self,n_inputs,n_neurons):
@@ -17,13 +20,43 @@ class Layer_Dense:
 class Activation_ReLU:
     def foward(self,inputs):
         self.output=np.maximum(0,inputs)
-layer1=Layer_Dense(2,5)
-layer1.foward(X)
+class Soft_Max:
+    def foward(self,inputs):
+        exp=np.exp(inputs-np.max(inputs,axis=1,keepdims=True))
+        prob=exp/np.sum(exp,axis=1,keepdims=True)
+        self.output=prob
+class Loss:
+    def calculate(self,output,y):
+        sample_losses=self.foward(output,y)
+        data_loss=np.mean(sample_losses)
+        return data_loss
+class Loss_CatCrosEnt(Loss):
+    def foward(self,y_pred,y_true):
+        samples=len(y_pred)
+        y_pred_clipped=np.clip(y_pred,1e-7, 1-(1e-7))
+        if len(y_true.shape)==1:
+            correct_confidences=y_pred_clipped[range(samples),y_true]
+        elif len(y_true.shape)==2:
+            correct_confidences=np.sum(y_pred_clipped*y_true,axis=1)
+        negative_log=np.log(correct_confidences)
+        return negative_log
+
+X,y= spiral_data(samples= 100,classes=3)
+
+dense1=Layer_Dense(2,3)
 activation1=Activation_ReLU()
-layer1.foward(X)
-activation1.foward(layer1.output)
-print(activation1.output)
+
+dense2=Layer_Dense(3,3)
+activation2=Soft_Max()
+
+dense1.foward(X)
+activation1.foward(dense1.output)
+
+dense2.foward(activation1.output)
+activation2.foward(dense2.output)
 
 
-inputs=[0,2,-1,3.3,-2.7,1.1,2.2,100]
+loss_fun=Loss_CatCrosEnt()
+loss=Loss.calculate(activation2.output, y)
 
+print(loss)
